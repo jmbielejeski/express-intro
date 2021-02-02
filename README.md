138ef90f5663b422607375c848decc08df48aa89
jmbielejeski <jmbielejeski@gmail.com>
2021-02-02 13:58:30 -0700

    started server

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 ## Express Intro
+
+npm init - in terminal
+npm install express
+npm install body-parser
+node package manager(npm) - for JS libraries
+
+control c - stops server
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// console.log(express);
+
+// Create out app
+const app = express();
+const port = 5000;
+
+// Listen for network request
+// couple of arguments - port, function
+app.listen(port, function () {
+  // when server is ready call this function
+  console.log(`I'm listening ...`, port);
+});
