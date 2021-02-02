1f4854d8cb3fbd626b7f583008f325b4ed8e44ff
jmbielejeski <jmbielejeski@gmail.com>
2021-02-02 14:50:49 -0700

    started server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// Share any files inside the public folder
+app.use(express.static('server/public'));
+
 // Listen for network request
 // couple of arguments - port, function
 app.listen(port, function () {
